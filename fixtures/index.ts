2c1c11c286ecd7a94947af7cbc852892fcc02e8d
Kamil Myśliwiec <mail@kamilmysliwiec.com>
2018-07-18 09:48:23 +0200

    feature: add fixtures directory

---
+++ fixtures/index.ts
@@ -0,0 +1,4 @@
+// tslint:disable:no-var-requires
+export const users = require('./users.json');
+export const organizations = require('./organizations.json');
+export const roles = require('./roles.json');
